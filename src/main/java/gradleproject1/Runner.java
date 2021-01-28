01337e4d3be35dbc44b5dc2f28fda3f2a51f75a5
Paul Kline <pauliankline@gmail.com>
2021-01-27 19:35:59 -0600

    first commit

---
+++ src/main/java/gradleproject1/Runner.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Paul
+ */
+public class Runner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Enter data using BufferReader
+//        BufferedReader reader
+//                = new BufferedReader(new InputStreamReader(System.in));
+//
+//        // Reading data using readLine
+//        String name;
+//        try {
+//            name = reader.readLine();
+//            System.out.println("You entered: " + name);
+//        } catch (IOException ex) {
+//            Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        // Using Scanner for Getting Input from User
+//        System.out.println("Now scanner test.");
+//        Scanner in = new Scanner(System.in);
+//        String s = in.nextLine();
+//        System.out.println("You entered string " + s);
+
+        System.out.println("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.println("Your username is " + username);
+
+    }
+
+}
---
+++ src/test/java/Tester1.java
@@ -0,0 +1,20 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Paul
+ */
+public class Tester1 {
+
+    @Test
+    public void testAdd() {
+        assertEquals(42, Integer.sum(19, 23));
+    }
+}
