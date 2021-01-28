14c20cd3d59bc6e28bf04bb6f372a85d69620157
Paul Kline <pauliankline@gmail.com>
2021-01-27 23:20:37 -0600

    my first commit

--- src/main/java/gradleproject1/Runner.java
+++ src/main/java/gradleproject1/Runner.java
@@ -17,6 +17,7 @@ public class Runner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         //Enter data using BufferReader
 //        BufferedReader reader
 //                = new BufferedReader(new InputStreamReader(System.in));
