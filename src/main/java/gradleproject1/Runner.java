d52b4cacd7f708faf4489f2ec9a6b3f67502a0a3
Paul Kline <pauliankline@gmail.com>
2021-01-28 00:21:00 -0600

    commit

--- src/main/java/gradleproject1/Runner.java
+++ src/main/java/gradleproject1/Runner.java
@@ -17,29 +17,6 @@ public class Runner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        //Enter data using BufferReader
-//        BufferedReader reader
-//                = new BufferedReader(new InputStreamReader(System.in));
-//
-//        // Reading data using readLine
-//        String name;
-//        try {
-//            name = reader.readLine();
-//            System.out.println("You entered: " + name);
-//        } catch (IOException ex) {
-//            Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        // Using Scanner for Getting Input from User
-//        System.out.println("Now scanner test.");
-//        Scanner in = new Scanner(System.in);
-//        String s = in.nextLine();
-//        System.out.println("You entered string " + s);
-
-        System.out.println("Enter your username: ");
-        Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextLine();
-        System.out.println("Your username is " + username);
 
     }
 
